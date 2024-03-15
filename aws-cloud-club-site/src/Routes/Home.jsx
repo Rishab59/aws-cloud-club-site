d9620e6175385b3e6bd6694d303e720f06225356
Rishab59 <hrishab59@gmail.com>
2024-03-15 17:46:46 +0530

    Added Components folder

--- aws-cloud-club-site/src/Routes/Home.jsx
+++ aws-cloud-club-site/src/Routes/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react" ;
 
 import { Button } from "react-bootstrap" ;
+import NavBar from "../Componenets/NavBar";
 
 
 function Home() {
     return (
         <div className = "pt-5">
+            <NavBar />
             Home
             <br />
             <Button>Testing Bootstrap</Button>
