e5af61bcd364db673ec61ffc149b2515f7f06dd8
Jeswin-J <jeswinjoseph164@gmail.com>
2024-03-24 21:17:23 +0530

    updates

--- aws-cloud-club-site/src/Components/TeamSection/TeamSection.jsx
+++ aws-cloud-club-site/src/Components/TeamSection/TeamSection.jsx
@@ -6,6 +6,10 @@ import SectionHeader from '../SectionHeader/SectionHeader';
 import "./TeamSection.css"
 
 import Jeswin from "../../assets/img/jeswin.jpg"
+import Porselvan from "../../assets/img/porselvan.jpg"
+import Sakkthivel from "../../assets/img/sakkthivel.jpeg"
+import Madhav from "../../assets/img/madhav.jpeg"
+import Rishab from "../../assets/img/rishab.jpeg"
 import TeamCard from '../TeamCard/TeamCard';
 
 function TeamSection() {
@@ -14,22 +18,30 @@ function TeamSection() {
       <SectionHeader header="Our Team" />
       <Row className='members-row-one'>
         <Col lg="5">
-            <TeamCard quote="Take the first step today<br /> In the stairs to Reach the Cloud" image={Jeswin} title="Madhavakumar D" designation="AWS Cloud Club Captain" />
+            <TeamCard 
+            quote="Take the first step today <br /> In the stairs to Reach the Cloud" 
+            image={Madhav} title="Madhavakumar D" 
+            designation="AWS Cloud Club Captain" />
         </Col>
       </Row>
       <Row className='members-row-two'>
         <Col lg="5">
-            <TeamCard quote="Take the first step today in the stairs to Reach the Cloud" image={Jeswin} title="Sakkthivel T"  designation="Technology Lead"/>
+            <TeamCard 
+            quote="The infrastructure of the future is being built by technology, with the architect, AWS cloud." 
+            image={Sakkthivel} title="Sakkthivel T"  
+            designation="Technology Lead"/>
         </Col>
         <Col lg="5" className='right-member'>
-            <TeamCard quote="Take the first step today in the stairs to Reach the Cloud" image={Jeswin} title="Porselvan P" designation="Aritificial Intelligence Lead"/>
+            <TeamCard quote="Scale Your Dreams with AI, <br /> Fuel Your Ambitions with AWS Cloud." 
+            image={Porselvan} title="Porselvan P" 
+            designation="Aritificial Intelligence Lead"/>
         </Col>
       </Row>
       
       <Row className='members-row-three'>
         <Col lg="5">
             <TeamCard quote="Just keep on increasing your FullStack. <br /> At one point, you will Reach the Cloud" 
-            image={Jeswin} 
+            image={Rishab} 
             title="Rishab H" 
             designation="Full-Stack Lead"/>
         </Col>
