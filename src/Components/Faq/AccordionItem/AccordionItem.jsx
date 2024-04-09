416b6ed91ae27ecf174d4c9873a25adaa935b14c
Jeswin-J <jeswinjoseph164@gmail.com>
2024-04-09 18:10:09 +0530

    URL Change

--- src/Components/Faq/AccordionItem/AccordionItem.jsx
+++ src/Components/Faq/AccordionItem/AccordionItem.jsx
@@ -4,7 +4,7 @@ import './AccordionItem.css';
 
 function AccordionItem(props) {
   return (
-    <Accordion defaultActiveKey="0" flush className="custom-accordion"> 
+    <Accordion className="custom-accordion"> 
       <Accordion.Item eventKey={props.eventKey} className="accordion-item">
         <Accordion.Header className="accordion-header">{props.question}</Accordion.Header>
         <Accordion.Body className="accordion-body">{props.answer}</Accordion.Body>
