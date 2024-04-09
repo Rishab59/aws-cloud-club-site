af756c02daba48e96134e50a5c0b33dbbb4ae580
Jeswin-J <jeswinjoseph164@gmail.com>
2024-04-09 16:57:29 +0530

    added faq

--- src/Routes/Join.jsx
+++ src/Routes/Join.jsx
@@ -3,6 +3,7 @@ import React from "react" ;
 import NavBar from "../Components/NavBar/NavBar" ;
 import Footer from "../Components/Footer/Footer";
 import JoinGuide from "../Components/JoinGuide/JoinGuide";
+import Faq from "../Components/Faq/Faq";
 
 
 function Join() {
@@ -10,6 +11,7 @@ function Join() {
         <div className = "Join-page">
             <NavBar />
             <JoinGuide />
+            <Faq />
             <Footer />
         </div>
     ) ;
