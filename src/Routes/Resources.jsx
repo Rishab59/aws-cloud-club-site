d189952ae2bd8fa63b1f36baec0a451e511c13db
Jeswin-J <jeswinjoseph164@gmail.com>
2024-04-09 21:36:13 +0530

    added global presence

--- src/Routes/Resources.jsx
+++ src/Routes/Resources.jsx
@@ -1,4 +1,4 @@
-import React from "react" ;
+import React, { useEffect } from "react" ;
 
 import NavBar from "../Components/NavBar/NavBar" ;
 import ResourcesSection from "../Components/ResoursesSection/ResourcesSection";
@@ -6,6 +6,9 @@ import Footer from "../Components/Footer/Footer";
 
 
 function Resources() {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+      }, []);
     return (
         <div className = "resources-page">
             <NavBar />
